4dfc1c942c306b9ef58f0e59f85b51bda706ba34
DumpAnswer <kaleksers93@gmail.com>
2021-03-02 14:32:30 +0300

    Второе задание

---
+++ Dz2/Task2/BinarSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class BinarSearch
+    {
+        
+
+        public static int binarSearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            {
+                while (min <= max)
+                {
+                    int mid = (min + max) / 2;
+                    if (searchValue < inputArray[mid])
+                    {
+                        return mid;
+                    }
+                    else if (searchValue < inputArray[mid])
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+                }
+                return -1;
+            }
+        }
+        
+    }
+}
---
+++ Dz2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            BinarSearch binarSearch = new BinarSearch();
+
+            int[] inputArray = new int[] { 1, 3, 5, 6, 7, 34, 64, 123, 43, 68, 32, 55, 66, 88 };
+
+                BinarSearch.binarSearch(inputArray, 64);
+
+
+            }
+        }
+    }
+
+

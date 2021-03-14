668fc38ec38f37bdd19f9031ad1454b3c28fc2f0
DumpAnswer <kaleksers93@gmail.com>
2021-03-15 00:55:16 +0300

    Домашняя работа №4
    Почти выполнил второе задание

---
+++ Dz4/Dz4 task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Dz4_task1
+{
+
+
+   public class Program
+    {
+      
+        
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+        public class BenchmarkClass
+        {
+            [Benchmark]
+            public void testHashSetFound()
+            {
+
+                HashSet<String> foundStr = new HashSet<string>();
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    Random rand = new Random();
+                    string value = Convert.ToString(rand.Next());
+                    foundStr.Add(value);
+                }
+
+                foundStr.Contains("Hello word");
+
+            }
+            [Benchmark]
+            public void testArrayFound()
+            {
+                string valueA = "12324214";
+                string[] arrayFound = new string[10000];
+                for (int i = 0; i < arrayFound.Length; i++)
+                {
+                    Random rand = new Random();
+                    string value = Convert.ToString(rand.Next());
+                    
+                }
+
+                foreach (string values in arrayFound)
+                {
+                    if(valueA == values)
+                    {
+                        break;
+                    }
+
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Dz4/Dz4Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dz4Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.AddItem(8);
+            tree.AddItem(10);
+            tree.AddItem(5);
+            tree.AddItem(3);
+            tree.AddItem(12);
+
+            tree.PrintTree();
+
+            
+
+           
+    
+            
+            
+           
+        }
+    }
+}
+
